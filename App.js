03368a7797f4bbc697ac8a0b4f93e4fb1ddcc0aa
khadidjabilinski630com-ops <khadidjabilinski630.com@gmail.com>
2026-01-16 21:26:42 +0100

    Fix App navigation imports

--- App.js
+++ App.js
@@ -1,35 +1,18 @@
-
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import ElementChoiceScreen from "./src/screens/ElementChoiceScreen";
-import HomeScreen from "./src/screens/HomeScreen";
-import PracticeScreen from "./src/screens/PracticeScreen";
-import BilanScreen from "./src/screens/BilanScreen";
+import ElementChoiceScreen from "./src/écrans/ElementChoiceScreen";
+import AccueilScreen from "./src/écrans/AccueilScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen
-          name="ElementChoice"
-          component={ElementChoiceScreen}
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-        />
-        <Stack.Screen
-          name="Practice"
-          component={PracticeScreen}
-        />
-        <Stack.Screen
-          name="Bilan"
-          component={BilanScreen}
-        />
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="ElementChoice">
+        <Stack.Screen name="ElementChoice" component={ElementChoiceScreen} />
+        <Stack.Screen name="Home" component={AccueilScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
